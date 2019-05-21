164e833a1fddd0f999967156ba699fc602697168
jonathanhand <hand.jonathan.13@gmail.com>
2019-05-21 05:34:56 -0700

    added buttons to switch styles

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-const resume = document.getElementById('resume');
+const resumeBtn = document.getElementById('resumeBtn');
+const projectBtn = document.getElementById('projectBtn');
+const projContent = document.getElementById('projContent');
+const resumeContent = document.getElementById('resumeContent')
+
+resumeBtn.addEventListener("click", resume);
+
+function resume() {
+    resumeBtn.style.display = '';
+    resumeContent.style.display = '';
+    projContent.style.display = 'none';
+    projectBtn.style.disply = '';
+}
+
+projectBtn.addEventListener("click", project);
+
+function project() {
+    resumeBtn.style.display = '';
+    resumeContent.style.display = 'none';
+    projContent.style.display = '';
+    projectBtn.style.disply = 'none';
+}
